155afa5c17597ae931cc17a632a3a3ad88453239
nio74 <mpaolo74@gmail.com>
2022-07-18 12:12:10 +0200

    lavoro18_7_12_10

--- js/google-map.js
+++ js/google-map.js
@@ -61,54 +61,22 @@ function init() {
 }
 google.maps.event.addDomListener(window, 'load', init); */
 
+
+
+  // Initialize and add the map
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: {
-        lat: 45.58581697906467,
-        lng: 11.859606636438022
-      },
-      zoom: 15,
-    });
-    const request = {
-      placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
-      fields: ["name", "formatted_address", "place_id", "geometry"],
-    };
-    const infowindow = new google.maps.InfoWindow();
-    const service = new google.maps.places.PlacesService(map);
-  
-    service.getDetails(request, (place, status) => {
-      if (
-        status === google.maps.places.PlacesServiceStatus.OK &&
-        place &&
-        place.geometry &&
-        place.geometry.location
-      ) {
-        const marker = new google.maps.Marker({
-          map,
-          position: place.geometry.location,
-        });
-  
-        google.maps.event.addListener(marker, "click", () => {
-          const content = document.createElement("div");
-          const nameElement = document.createElement("h2");
-  
-          nameElement.textContent = place.name;
-          content.appendChild(nameElement);
-  
-          const placeIdElement = document.createElement("p");
-  
-          placeIdElement.textContent = place.place_id;
-          content.appendChild(placeIdElement);
-  
-          const placeAddressElement = document.createElement("p");
-  
-          placeAddressElement.textContent = place.formatted_address;
-          content.appendChild(placeAddressElement);
-          infowindow.setContent(content);
-          infowindow.open(map, marker);
-        });
-      }
-    });
-  }
-  
-  window.initMap = initMap;
+  // The location of Uluru
+  const uluru = { lat: 45.58581697906467, lng: 11.859606636438022 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
